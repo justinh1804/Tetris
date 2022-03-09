891398503a94b3956173843a486ac83741cb8007
justin horton <justinh1804@gmail.com>
2022-03-09 09:49:54 +0200

    final

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public Text highscore_num;
+
+    private void Start()
+    {
+        highscore_num.text = PlayerPrefs.GetInt("Highscore", 0).ToString();
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public static class Score
+{
+    public static int score_num = 0;
+
+    public static void IncreaseScore(Text Score_num)
+    {
+        Score.score_num++;
+        Score_num.text = Score.score_num.ToString();
+
+        if (Score.score_num > PlayerPrefs.GetInt("Highscore", 0))
+        {
+            PlayerPrefs.SetInt("Highscore", Score.score_num);
+        }
+    }
+}
